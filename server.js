1d73dcaae5faecca0b81030e52de0c5bfdfc2284
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-11-10 14:09:27 -0300

    refactor: add express lib

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-  "/": "Bookstore API"
-};
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end(rotas[req.url]);
+// });
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Server running at http://localhost:3000/")
 });
