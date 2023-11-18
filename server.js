c4fd137b63ae626fdc5adc3f80bb56bcec63f3bd
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-11-18 12:34:01 -0300

    feat: add CRUD functions

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-// import http from "http";
 import app from "./src/app.js";
 
 const PORT = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { "Content-Type": "text/plain" });
-//   res.end(rotas[req.url]);
-// });
 
 app.listen(PORT, () => {
   console.log("Server running at http://localhost:3000/")
--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const albums = [
+  {
+    id: 1,
+    titulo: "AM"
+  },
+  {
+    id: 2,
+    titulo: "Favourite Worst Nightmare"
+  }
+]
+
+function findAlbum(id) {
+  return albums.findIndex(albums => {
+    return albums.id === Number(id);
+  });
+}
+
+
+app.get("/albums", (req, res) => {
+  res.status(200).json(albums);
+});
+
+app.get("/albums/:id", (req, res) => {
+  const index = findAlbum(req.params.id);
+  res.status(200).json(albums[index]);
+});
+
+app.post("/albums", (req, res) => {
+  albums.push(req.body);
+  res.status(201).send("CD adicionado com sucesso!");
+});
+
+app.put("/albums/:id", (req, res) => {
+  const index = findAlbum(req.params.id); 
+  albums[index].titulo = req.body.titulo;
+  res.status(200).json(albums[index]);
 });
 
 export default app;
